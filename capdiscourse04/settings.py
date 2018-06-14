3ecb85188a5ca68381765a1fb96701e26dfd0578
Freedom <froggiedevelopment@gmail.com>
2018-06-13 18:39:47 -0700

    changed allowed hosts

--- capdiscourse04/settings.py
+++ capdiscourse04/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR, 'capdiscourse04', 'etc', 'secret.txt')) as f:
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '206.189.175.168', '127.0.0.1'
+    '206.189.175.168', '127.0.0.1', 'ubuntu-s-1vcpu-1gb-sfo2-01-capdiscourse01'
 ]
 
 
